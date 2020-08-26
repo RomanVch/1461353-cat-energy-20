f618d24e504a27ba66d211d757980f415c89d68b
RomanVch <RomanV92@yandex.ru>
2020-08-26 14:33:54 +0500

    index js

--- source/js/active.js
+++ source/js/active.js
@@ -1,12 +1,12 @@
 var buttonss = document.querySelector(".form__button");
-var name = document.querySelector(".form__input--name");
+var namess = document.querySelector(".form__input--name");
 var email = document.querySelector(".form__input--weight");
 var weight= document.querySelector(".form__input--email");
 var tel = document.querySelector(".form__input--tel");
-var form = document.querySelector(".form__tag")
+var form = document.querySelector(".form__tag");
 
 form.addEventListener("submit",function (evt) {
-  if(!name.value || !weight.value || !tel.value) {
+  if(!namess.value) {
   evt.preventDefault();
-  console.log("привет тупица");}
+  namess.classList.add("form__input-error") ;}
 });
