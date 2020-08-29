72f2633df01c6e13b641ce1d1dda5eb7c2aa8965
RomanVch <RomanV92@yandex.ru>
2020-08-30 00:29:21 +0500

    revisions1

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,11 +1,11 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.page-header__toggle');
 
-  navMain.classList.remove('main-nav--nojs');
-  navMain.classList.remove('main-nav--opened');
-  navMain.classList.add('main-nav--closed');
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
 
-navToggle.addEventListener('click', function() {
-  navMain.classList.toggle('main-nav--closed');
-    navMain.classList.toggle('main-nav--opened');
+navToggle.addEventListener('click', function () {
+	navMain.classList.toggle('main-nav--closed');
+	navMain.classList.toggle('main-nav--opened');
 });
