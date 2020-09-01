ab6d9a50d4eaface59a541596a088709881e102e
RomanVch <RomanV92@yandex.ru>
2020-09-01 18:04:00 +0500

    fix v0.9

--- source/js/active.js
+++ source/js/active.js
@@ -5,38 +5,38 @@ var email = document.querySelector(".form__input--email");
 var tel = document.querySelector(".form__input--tel");
 var form = document.querySelector(".form__tag");
 
-form.addEventListener("submit", function (evt) {
-	if (!namess.value) {
-		evt.preventDefault();
-		namess.classList.add("form__input--error");
-	} else {
-		namess.classList.remove("form__input--error");
-	}
+form.addEventListener("submit", function(evt) {
+  if (!namess.value) {
+    evt.preventDefault();
+    namess.classList.add("form__input--error");
+  } else {
+    namess.classList.remove("form__input--error");
+  }
 });
 
-form.addEventListener("submit", function (evt) {
-	if (!weight.value) {
-		evt.preventDefault();
-		weight.classList.add("form__input--error");
-	} else {
-		weight.classList.remove("form__input--error");
-	}
+form.addEventListener("submit", function(evt) {
+  if (!weight.value) {
+    evt.preventDefault();
+    weight.classList.add("form__input--error");
+  } else {
+    weight.classList.remove("form__input--error");
+  }
 });
 
-form.addEventListener("submit", function (evt) {
-	if (!email.value) {
-		evt.preventDefault();
-		email.classList.add("form__input--error");
-	} else {
-		email.classList.remove("form__input--error");
-	}
+form.addEventListener("submit", function(evt) {
+  if (!email.value) {
+    evt.preventDefault();
+    email.classList.add("form__input--error");
+  } else {
+    email.classList.remove("form__input--error");
+  }
 });
 
-form.addEventListener("submit", function (evt) {
-	if (!tel.value) {
-		evt.preventDefault();
-		tel.classList.add("form__input--error");
-	} else {
-		tel.classList.remove("form__input--error");
-	}
+form.addEventListener("submit", function(evt) {
+  if (!tel.value) {
+    evt.preventDefault();
+    tel.classList.add("form__input--error");
+  } else {
+    tel.classList.remove("form__input--error");
+  }
 });
--- source/js/menu.js
+++ source/js/menu.js
@@ -5,7 +5,7 @@ navMain.classList.remove('main-nav--nojs');
 navMain.classList.remove('main-nav--opened');
 navMain.classList.add('main-nav--closed');
 
-navToggle.addEventListener('click', function () {
-	navMain.classList.toggle('main-nav--closed');
-	navMain.classList.toggle('main-nav--opened');
+navToggle.addEventListener('click', function() {
+  navMain.classList.toggle('main-nav--closed');
+  navMain.classList.toggle('main-nav--opened');
 });
